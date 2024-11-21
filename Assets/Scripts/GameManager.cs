dc29d9c2a3208808b339d0abf0abc230e2b83509
Rumia <lemmings1010@yahoo.co.kp>
2024-11-21 14:07:02 +0900

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    int score=0;
+    public TextMeshProUGUI scoreText;
+    public GameObject msgText;
+    public void IncreaseScore(){
+        score++;
+        scoreText.text=score.ToString();
+    }
+    public void GameEnd(){
+        msgText.SetActive(true);
+        Time.timeScale=0f;
+    }
+}
